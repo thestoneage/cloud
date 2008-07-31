d5dd4ef33f087d5de37a872a8c06bf20fcfbd9cc
Marc Rummel <marc@MacBook.local>
2008-07-31 14:50:24 +0200

    Implementation of init_population, using a Class based interface

--- lib/genetic.rb
+++ lib/genetic.rb
@@ -1,7 +1,20 @@
 class Genetic
-  def initialize(args)
-    
+  
+  attr_reader :population_size, :max_generations, :generation, :genome_type
+  
+  def initialize(population_size, max_generations, genome_type)
+    @population_size = population_size
+    @max_generations = max_generations
+    @generation = 0
+    @genome_type = genome_type
   end
   
+  def init_population
+    @population = []
+    @population_size.times do 
+      @population << genome_type.new
+    end
+    return @population
+  end
   
 end
--- test/test_genetic.rb
+++ test/test_genetic.rb
@@ -3,7 +3,28 @@ require File.join(File.dirname(__FILE__), 'cloud_test_helper')
 require "genetic"
 
 class TestGeneticAlgorithem < Test::Unit::TestCase
-  def test_case_name
-    
+  
+  def test_initialization
+    population_size = 20
+    generations = 10
+    genome_type = mock()
+    genome_type.expects(:class).returns(Class)
+    genetic = Genetic.new(population_size, generations, genome_type)
+
+    assert_equal(population_size, genetic.population_size)
+
+    assert_equal(generations, genetic.max_generations)
+
+    assert_equal(0, genetic.generation)
+
+    assert(genetic.genome_type.class == Class, "Genome Type is no Class Object.")
   end
+
+  def test_case_init_population
+    genome_type = mock()
+    genome_type.expects(:new).times(10)
+    genetic = Genetic.new(10, 10, genome_type)
+    assert_equal(10, genetic.init_population.size)
+  end
+  
 end
