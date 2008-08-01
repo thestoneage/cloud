3112932a97008f0cec9fddca906552cb88b2fb7c
Marc Rummel <marc@MacBook.local>
2008-08-01 12:01:16 +0200

    Implementation/Test of compute_fitness

--- lib/genetic.rb
+++ lib/genetic.rb
@@ -1,20 +1,35 @@
 class Genetic
   
-  attr_reader :population_size, :max_generations, :generation, :genome_type
+  attr_reader :population_size, :population, :max_generations, :generation, :genome_type, :selector
+  attr_writer :selector, :population
   
-  def initialize(population_size, max_generations, genome_type)
+  def initialize(population_size, max_generations, genome_type, selector)
     @population_size = population_size
     @max_generations = max_generations
     @generation = 0
     @genome_type = genome_type
+    @selector = selector
   end
-  
+
   def init_population
     @population = []
-    @population_size.times do 
-      @population << genome_type.new
+    @population_size.times do
+      genome = genome_type.new
+      genome.random_init
+      @population << genome
     end
     return @population
   end
-  
+
+  def optimize
+    @max_generations.times do
+      compute_fitness
+      @selector.select
+    end 
+  end
+
+  def compute_fitness
+    @population.each { |chromosome| chromosome.compute_fitness }
+  end
+
 end
--- test/test_genetic.rb
+++ test/test_genetic.rb
@@ -3,28 +3,47 @@ require File.join(File.dirname(__FILE__), 'cloud_test_helper')
 require "genetic"
 
 class TestGeneticAlgorithem < Test::Unit::TestCase
-  
+
   def test_initialization
     population_size = 20
     generations = 10
     genome_type = mock()
-    genome_type.expects(:class).returns(Class)
-    genetic = Genetic.new(population_size, generations, genome_type)
+    selector = mock()
+    genetic = Genetic.new(population_size, generations, genome_type, selector)
 
     assert_equal(population_size, genetic.population_size)
-
     assert_equal(generations, genetic.max_generations)
-
     assert_equal(0, genetic.generation)
-
-    assert(genetic.genome_type.class == Class, "Genome Type is no Class Object.")
+    assert_equal(selector, genetic.selector)
+    assert_equal(genome_type, genetic.genome_type)
   end
 
   def test_case_init_population
     genome_type = mock()
-    genome_type.expects(:new).times(10)
-    genetic = Genetic.new(10, 10, genome_type)
-    assert_equal(10, genetic.init_population.size)
+    genome = mock()
+    genome.expects(:random_init).times(10)
+    genome_type.expects(:new).times(10).returns(genome)
+    genetic = Genetic.new(10, 10, genome_type, mock())
+    genetic.init_population
+    assert_equal(10, genetic.population.size)
+    assert_equal(genome, genetic.population.first)
+    assert_equal(genome, genetic.population.last)
   end
   
+  def test_optimize
+    selector = mock()
+    selector.expects(:select).times(10)
+    genetic = Genetic.new(20, 10, mock(), selector)
+    genetic.optimize
+  end
+
+  def test_compute_fitness
+    genetic = Genetic.new(20, 10, mock(), mock())
+    chromosome1 = mock()
+    chromosome1.expects(:compute_fitness)
+    chromosome2 = mock()
+    chromosome2.expects(:compute_fitness)
+    genetic.population = [chromosome1, chromosome2]
+    genetic.compute_fitness
+  end
 end
